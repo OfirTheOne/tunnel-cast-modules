f5f5ad2608449578c3bfeee7f2ffd41c0d7c9ec4
Ofir Genish <ofir3322@gmail.com>
2020-08-16 23:32:35 +0300

    export options.set object

--- packages/tunnel-cast/lib/core/decorator/field-option-setter/index.ts
+++ packages/tunnel-cast/lib/core/decorator/field-option-setter/index.ts
@@ -1,27 +1,6 @@
-import { BaseFieldOptions } from "../../../model/field-options";
-import { getFieldDefinitionFromScheme } from "../../model-metadata/extract-metadata";
+import { FieldOptionSetterDecoratorFactory } from './factory'
 
 
-
-
-
-export function FieldOptionSetterDecoratorFactory<OP extends BaseFieldOptions>(
-    optionsKey: keyof OP, 
-    optionsValue: OP[keyof OP]
-) {
-    return function(prototype: any, key: string) {
-        // const embedData = getFieldDefinitionFromScheme(
-        //     prototype,
-        //     key,
-        // );
-
-        // if(embedData) {
-        //     embedData[0].options
-        // }
-        // embedMetadataOptions(
-        //     optionsKey as string,
-        //     optionsValue
-        // );
-    };
+export const options = {
+    set: FieldOptionSetterDecoratorFactory
 }
-
