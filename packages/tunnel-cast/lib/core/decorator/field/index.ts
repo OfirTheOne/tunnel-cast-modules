1ff50b0043de85dbbec195838ab39bfa95998278
Ofir Genish <ofir3322@gmail.com>
2020-08-16 23:34:08 +0300

    export options.set from field object

--- packages/tunnel-cast/lib/core/decorator/field/index.ts
+++ packages/tunnel-cast/lib/core/decorator/field/index.ts
@@ -5,10 +5,14 @@ import { boolean } from './boolean'
 import { array } from './array'
 import { model } from './model'
 
+import { options } from './../field-option-setter'
+
 export const field = {
     Number: number,
     String: string,
     Boolean: boolean,
     Array: array,
-    Model: model
+    Model: model,
+    //
+    options
 }
