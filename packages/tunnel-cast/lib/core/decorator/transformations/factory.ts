a20ab2aaab2e22ba329f0bcb73608d28cd63630a
Ofir Genish <ofir3322@gmail.com>
2020-08-16 23:33:14 +0300

    change function name

--- packages/tunnel-cast/lib/core/decorator/transformations/factory.ts
+++ packages/tunnel-cast/lib/core/decorator/transformations/factory.ts
@@ -1,12 +1,12 @@
 
 
-import { getFieldDefinitionFromScheme } from "../../model-metadata/extract-metadata";
+import { getFieldDefinitionFromPrototype } from "../../model-metadata/extract-metadata";
 
 
 export function FieldTransformationsDecoratorFactory(transformations: Function) {
 
     return function(prototype: any, key: string) {
-        const embedData = getFieldDefinitionFromScheme(
+        const embedData = getFieldDefinitionFromPrototype(
             prototype,
             key,
         );
