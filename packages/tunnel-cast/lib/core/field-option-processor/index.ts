57b2b2214fe241e8d6c32fb36fa5e9a6922ab878
Ofir Genish <ofir3322@gmail.com>
2020-08-16 23:26:42 +0300

    separate the options-process to own classes

---
+++ packages/tunnel-cast/lib/core/field-option-processor/index.ts
@@ -0,0 +1,64 @@
+
+import * as OPS from './../../model/field-options'
+import { globals } from '../../globals';
+
+export class FieldOptionProcessor<T extends OPS.BaseFieldOptions = OPS.BaseFieldOptions> {
+    process(options: T, fieldName: string) {
+        let required = options.required == undefined ?
+            (options.nullable != undefined ? !options.nullable : globals.FIELD_REQUIRED_DEFAULT) :
+            options.required;
+
+        let nullable = !required
+
+        return {
+            attribute: options.attribute || fieldName,
+            validate: options.validate,
+            transformations: options.transformations || [],
+            validations: options.validations || [],
+            parsing: options.parsing || [],
+            required,
+            nullable,
+            nullableIf: options.nullableIf,
+            requiredIf: options.requiredIf,
+            error: options.error,
+            default: options.default
+        }
+    }
+}
+
+
+export class NumberFieldOptionProcessor extends FieldOptionProcessor<OPS.NumberFieldOptions> {
+    
+    process(options: OPS.NumberFieldOptions, fieldName: string) {
+        const baseOptions = super.process(options, fieldName);
+        return { ...options, ...baseOptions }
+    }
+}
+
+export class StringFieldOptionProcessor extends FieldOptionProcessor {
+    process(options: OPS.StringFieldOptions, fieldName: string) {
+        const baseOptions = super.process(options, fieldName);
+        return { ...options, ...baseOptions }
+    }
+}
+
+export class BooleanFieldOptionProcessor extends FieldOptionProcessor {
+    process(options: OPS.BooleanFieldOptions, fieldName: string) {
+        const baseOptions = super.process(options, fieldName);
+        return { ...options, ...baseOptions }
+    }
+}
+
+export class ArrayFieldOptionProcessor extends FieldOptionProcessor {
+    process(options: OPS.ArrayFieldOptions, fieldName: string) {
+        const baseOptions = super.process(options, fieldName);
+        return { ...options, ...baseOptions }
+    }
+}
+
+export class ModelFieldOptionProcessor extends FieldOptionProcessor {
+    process(options: OPS.ModelFieldOptions, fieldName: string) {
+        const baseOptions = super.process(options, fieldName);
+        return { ...options, ...baseOptions }
+    }
+}
