7a07f40e5fe378a17917e563fe18d827ce857b7e
Ofir Genish <ofir3322@gmail.com>
2020-08-16 23:33:02 +0300

    create options setter decorator factory

---
+++ packages/tunnel-cast/lib/core/decorator/field-option-setter/factory.ts
@@ -0,0 +1,22 @@
+import { BaseFieldOptions } from "../../../model/field-options";
+import { getFieldDefinitionFromPrototype } from "../../model-metadata/extract-metadata";
+
+
+
+
+
+export function FieldOptionSetterDecoratorFactory(
+    optionKey: string, 
+    value: any
+) {
+    return function(prototype: any, key: string) {
+        const embedData = getFieldDefinitionFromPrototype(prototype, key);
+        
+        if(embedData) {
+            Array.isArray(embedData[0].options[optionKey]) ? 
+                (embedData[0].options[optionKey]).push(value) : 
+                embedData[0].options[optionKey] = value;
+        }
+    };
+}
+
