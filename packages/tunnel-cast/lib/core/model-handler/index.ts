b81215670279ff4e7f6ebeb761741b89201294b7
Ofir Genish <ofir3322@gmail.com>
2020-09-01 18:29:15 +0300

    execure cast in the parsing stage

--- packages/tunnel-cast/lib/core/model-handler/index.ts
+++ packages/tunnel-cast/lib/core/model-handler/index.ts
@@ -1,16 +1,17 @@
-import { BaseFieldOptions } from "../../model";
+import { BaseFieldOptions, ModelFieldOptions } from "../../model";
 import { FieldHandler } from "../field-handler";
 import { NativeValidationDict } from "../../model/native-validation-dict";
 import { Class } from "../../utils/model";
 import { cast } from '../cast'
+import { options } from "../decorator/field-option-setter";
 
 
 
 export const FieldTypeId = "MODEL" 
 
-export class ModelFieldHandler extends FieldHandler<BaseFieldOptions> {
+export class ModelFieldHandler extends FieldHandler<ModelFieldOptions> {
 
-    nativeValidations : NativeValidationDict<BaseFieldOptions> = {
+    nativeValidations : NativeValidationDict<ModelFieldOptions> = {
     };
 
 
@@ -18,24 +19,34 @@ export class ModelFieldHandler extends FieldHandler<BaseFieldOptions> {
         super(context, fieldName, projectedContext)
     }
 
-    protected extractValue(options: BaseFieldOptions) {
-        const value = super.extractValue(options)
+    // protected extractValue(options: ModelFieldOptions) {
+    //     const value = super.extractValue(options)
+    //     const castValue = cast(this.modelClass, value);
+        
+    // }
+
+    protected runParsing(value: any, parsing: Function[]) {
         const castValue = cast(this.modelClass, value);
         if('errors' in castValue) {
-            throw castValue
-        } else {
-            return castValue.value;
-        }
+            throw castValue.errors
+        } 
+        return super.runParsing(castValue, parsing)
+
     }
 
-    typeCondition(value: any, options: BaseFieldOptions): boolean {
-        return true;
+    typeCondition(value: any, options: ModelFieldOptions): boolean {
+        return typeof value == 'object' && value != null ;
     }
 
-    processOption(options: BaseFieldOptions): BaseFieldOptions {
+    processOption(options: ModelFieldOptions): ModelFieldOptions {
         const baseOptions = super.processOption(options);
         return { ...options, ...baseOptions }
     }
+    
+    // processError(casteError: any, options: ModelFieldOptions) {
+
+    //     Object.assign(casteError, { options, fieldName: this.fieldName })
+    // }
 }
 
 
