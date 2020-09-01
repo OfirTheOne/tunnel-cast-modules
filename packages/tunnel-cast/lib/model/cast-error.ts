7475d900b367601e7ee2c4a45337d4918fea0b94
Ofir Genish <ofir3322@gmail.com>
2020-09-01 18:27:45 +0300

    define error structure

---
+++ packages/tunnel-cast/lib/model/cast-error.ts
@@ -0,0 +1,15 @@
+
+export interface DebugCastError {
+    errors: Array<CastError | Error>
+    fieldName: string,
+    value: any,
+    options: any,
+}
+
+
+export interface LeanCastError {
+    errors: Array<CastError | Error>
+}
+
+
+export type CastError = DebugCastError | LeanCastError 
