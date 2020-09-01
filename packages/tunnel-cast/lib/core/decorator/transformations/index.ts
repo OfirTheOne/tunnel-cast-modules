3c76477d3b628fca227a7c44e235b2b72c44aa7a
Ofir Genish <ofir3322@gmail.com>
2020-09-01 18:29:57 +0300

    export decorators

--- packages/tunnel-cast/lib/core/decorator/transformations/index.ts
+++ packages/tunnel-cast/lib/core/decorator/transformations/index.ts
@@ -1,7 +1,11 @@
 
-
+import { parseInt, parseFloat } from './number-parse'
+import { mathCeil, mathFloor } from './math'
 
 
 export const transformations = {
-
+    parseInt,
+    parseFloat,
+    mathCeil, 
+    mathFloor
 }
