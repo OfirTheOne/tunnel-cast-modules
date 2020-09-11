3da38f50deb1131af60a1f60331b82c12b9a08fe
Ofir Genish <ofir3322@gmail.com>
2020-09-11 23:01:55 +0300

    add inline docs

--- packages/tunnel-cast/lib/core/decorator/field/index.ts
+++ packages/tunnel-cast/lib/core/decorator/field/index.ts
@@ -10,12 +10,35 @@ import { options } from './../field-option-setter'
 import { assert } from './assert';
 
 export const field = {
+    /**
+     * @description declare a Number type 
+     */
     Number: number,
+    /**
+     * @description declare a String type
+     */
     String: string,
+    /**
+     * @description declare a Boolean type
+     */
     Boolean: boolean,
+    /**
+     * @description declare a Array type
+     */
     Array: array,
+    /**
+     * @description declare a Model type, inferred the mode class from the field type definition. 
+     */
     Model: model,
-    //
+
+    // ==== //
+    
+    /**
+     * @description declare the type (primitive / class) of the origin field value, and assert it
+     */
     Assert: assert,
+    /**
+     * @description provide access to the field-type's options object.
+     */
     options
 }
