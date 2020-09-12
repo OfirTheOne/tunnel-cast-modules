15218f587cac0c887ae241bbfe1e39e75c4e1158
Ofir Genish <ofir3322@gmail.com>
2020-09-13 00:00:08 +0300

    add logger setting

--- packages/tunnel-cast/lib/globals/index.ts
+++ packages/tunnel-cast/lib/globals/index.ts
@@ -1,10 +1,18 @@
 
 
+import { Logger } from './../utils/logger'
+
 
 export const globals = {
-    STOP_ON_FIRST_FAIL: false,
-    DEBUG_ERROR: true,
-    FIELD_REQUIRED_DEFAULT: true,
-    FIELD_NULLABLE_DEFAULT: false,
-    FIELD_VALIDATE_DEFAULT: true,
+    STOP_ON_FIRST_FAIL:  false,
+    DEBUG_ERROR:  true,
+    FIELD_REQUIRED_DEFAULT:  true,
+    FIELD_NULLABLE_DEFAULT:  false,
+    FIELD_VALIDATE_DEFAULT:  true,
+    VERBOSE_LEVEL:  0,
+    LOGGER_STDOUT: process.stdout,
+    LOGGER: undefined as Logger
 }
+
+globals['LOGGER'] = new Logger(globals)
+
