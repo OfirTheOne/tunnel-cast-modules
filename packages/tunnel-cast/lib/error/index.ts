b673cdf83ee4c1c42fe102837de89e32c4488b23
Ofir Genish <ofir3322@gmail.com>
2020-09-13 00:00:44 +0300

    implement costum errors

--- packages/tunnel-cast/lib/error/index.ts
+++ packages/tunnel-cast/lib/error/index.ts
@@ -1,13 +1,80 @@
 
 
 
-export class ModelMetadataRepoNotFoundError extends Error {
 
-    public code = 'ModelMetadataRepoNotFound';
+
+export class TunnelCastError  extends Error {
+
+    constructor(message: string, public code: string, description?: string) {
+        super(message);
+    }
+}
+
+
+
+export class ModelMetadataRepoNotFoundError extends TunnelCastError {
 
     constructor(modelName: string) {
         super(
-            `Metadata repository not found in "${modelName}."`
+            `Metadata repository not found in "${modelName}."`,
+            `ModelMetadataRepoNotFoundError`,
         );
     }   
 }
+
+
+
+export class CastingError extends TunnelCastError {
+
+    constructor(
+        message: string, 
+        code: string, 
+        context?: { modelName: string, fieldName: string, fieldOptions: any, parentRef: any}
+    ) {
+        super(
+            message,
+            code
+        );
+    }   
+}
+
+export class FieldRequiredError extends CastingError {
+
+    constructor(modelName: string) {
+        super(
+            `Metadata repository not found in "${modelName}."`,
+            'FieldRequiredError'
+        );
+    }   
+}
+
+export class AssertError extends CastingError {
+
+    constructor() {
+        super(
+            `Assertion failed."`,
+            'AssertError'
+        );
+    }   
+}
+
+export class TypeConditionError extends CastingError {
+
+    constructor(modelName: string) {
+        super(
+            `Type condition failed."`,
+            'TypeConditionError'
+        );
+    }   
+}
+
+export class TypeValidationError extends CastingError {
+
+    constructor(modelName: string) {
+        super(
+            `Metadata repository not found in "${modelName}."`,
+            'TypeValidationError'
+        );
+    }   
+}
+
