2a1b5c03b9367360cd49b84f314255afa2525b3c
Ofir Genish <ofir3322@gmail.com>
2020-09-13 00:00:28 +0300

    add logging to cast method

--- packages/tunnel-cast/lib/core/cast/index.ts
+++ packages/tunnel-cast/lib/core/cast/index.ts
@@ -6,14 +6,17 @@ import { CastResolve } from '../../model/public/cast-resolve';
 import { FieldEmbeddedData } from '../../model/inner/field-embedded-data';
 import { FieldHandler } from '../../core/field-handler/field-handler';
 import { globals } from "../../globals";
-
+import { VerboseLevel } from '../../utils/logger'
 
 export function cast<T>(Model: Class<T>, target: Record<any, any>): CastResolve<T> {
 
+    const logger = globals['LOGGER'];
     const repo: Map<string, Array<FieldEmbeddedData>> = extractRootRepo(Model)
     const fieldDefinitions = getFieldDefinitions(repo).map(entry=> entry[0]);
     const projectedContext = {};
     const errors = [];
+
+    logger.log(`run cast on "${Model.name}" model, with ${fieldDefinitions.length} definitions`, VerboseLevel.Low);
 
     for(let def of fieldDefinitions) {
         const handler: FieldHandler = new def.fieldHandlerClass(
@@ -22,8 +25,13 @@ export function cast<T>(Model: Class<T>, target: Record<any, any>): CastResolve<
             projectedContext,
             ...(def.handlerArgs||[])
         );
+
+        logger.log(`handling "${def.fieldKey}" field definition`, VerboseLevel.Medium);
+
         const handlerResult = handler.handle(def.options);
         if('errors' in handlerResult) {
+            logger.log(`error occurred on "${def.fieldKey}" field definition`, VerboseLevel.Medium);
+
             errors.push(handlerResult)
             if(globals.STOP_ON_FIRST_FAIL) {
                 break;
