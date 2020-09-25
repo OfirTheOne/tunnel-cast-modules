519b2eb73edac0139084267d50691c4e3089c13f
Ofir Genish <ofir3322@gmail.com>
2020-09-25 19:11:55 +0300

    add handlerAsArray options to use for parsing, validations and transformations

--- packages/tunnel-cast/lib/common/decorator/field-options/option-setter/factory.ts
+++ packages/tunnel-cast/lib/common/decorator/field-options/option-setter/factory.ts
@@ -3,15 +3,21 @@ import { getFieldDefinitionFromPrototype } from "../../../../internal/model-meta
 
 export function FieldOptionSetterDecoratorFactory(
     optionKey: string, 
-    value: any
+    value: any,
+    handlerAsArray: boolean = false
 ) {
     return function(prototype: any, key: string) {
         const embedData = getFieldDefinitionFromPrototype(prototype, key);
         
         if(embedData) {
-            Array.isArray(embedData[0].options[optionKey]) ? 
-                (embedData[0].options[optionKey]).push(value) : 
+            if(handlerAsArray) {
+                Array.isArray(embedData[0].options[optionKey]) ? 
+                    (embedData[0].options[optionKey]).push(value) : 
+                    (embedData[0].options[optionKey] = [value]); 
+            } else {
                 embedData[0].options[optionKey] = value;
+
+            }
         }
     };
 }
