e36403c915deb8c2ef0663bf90b03769e1e727d7
Ofir Genish <ofir3322@gmail.com>
2020-10-19 00:27:58 +0300

    change file names to dot seperator format

--- packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.factory.ts
+++ packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.factory.ts
@@ -1,8 +1,8 @@
 
 import { Type } from '@nestjs/common'
 import { Request as eRequest, Response as eResponse } from 'express'
-import { CastPipe } from '../pipes'
-import { CastParameterDecorator } from './cast-parameter-decorator'
+import { CastPipeFactory, CastPipe } from '../pipes'
+import { CastParameterDecorator } from './cast.parameter-decorator'
 
 
 export function CastParameterDecoratorFactory(from: "req", fieldName: keyof eRequest): <T>(model: Type<T>) => ParameterDecorator;
@@ -11,6 +11,16 @@ export function CastParameterDecoratorFactory(from: "res"): (model: any) => Para
 export function CastParameterDecoratorFactory(from: "req"|"res", fieldName?: string): <T>(model: Type<T>) => ParameterDecorator  {
     return <T>(model: Type<T>) => {
       const decorator = CastParameterDecorator<T>(from, fieldName)
-      return decorator(CastPipe(model))
+      return decorator(CastPipeFactory(model))
     } 
+}
+
+
+
+export function CastParameterDecoratorInferFactory(from: "req", fieldName: keyof eRequest): ParameterDecorator;
+export function CastParameterDecoratorInferFactory(from: "res", fieldName: keyof eResponse): ParameterDecorator;
+export function CastParameterDecoratorInferFactory(from: "res"): (model: any) => ParameterDecorator;
+export function CastParameterDecoratorInferFactory(from: "req"|"res", fieldName?: string): ParameterDecorator  {
+  const decorator = CastParameterDecorator(from, fieldName)
+  return decorator(CastPipe)
 }
--- packages/nestjs-tunnel-cast/lib/pipes/index.ts
+++ packages/nestjs-tunnel-cast/lib/pipes/index.ts
@@ -1,1 +1,2 @@
-export { CastPipe } from './cast.pipe';
+export { CastPipeFactory } from './cast.pipe.factory';
+export { CastPipe } from './cast.pipe'
