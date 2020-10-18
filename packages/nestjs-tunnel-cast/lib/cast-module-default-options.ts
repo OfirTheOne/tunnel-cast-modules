5703a25364acd6b6341f6969209ddfe992c6f0b5
Ofir Genish <ofir3322@gmail.com>
2020-10-19 00:41:10 +0300

    define module default options

---
+++ packages/nestjs-tunnel-cast/lib/cast-module-default-options.ts
@@ -0,0 +1,8 @@
+import { InvalidCastException } from "./errors";
+import { CastModuleOptions } from "./interfaces";
+import { transformLeanError } from "./utils/transform-lean-error";
+
+export const defaultOptions: CastModuleOptions = {
+    castError: InvalidCastException,
+    transformError: transformLeanError
+}
