e2fe2cffa23a4030b9a05df327161f5f11e45a92
Ofir Genish <ofir3322@gmail.com>
2020-10-19 00:26:44 +0300

    add temp infer decorators

--- packages/nestjs-tunnel-cast/lib/parameter-decorators/common-cast-parameter-decorator.ts
+++ packages/nestjs-tunnel-cast/lib/parameter-decorators/common-cast-parameter-decorator.ts
@@ -1,5 +1,13 @@
-import { CastParameterDecoratorFactory } from "./cast-parameter-decorator-factory";
+import { 
+    CastParameterDecoratorFactory,
+    CastParameterDecoratorInferFactory
+} from "./cast.parameter-decorator.factory";
 
 export const CastQuery = CastParameterDecoratorFactory('req', 'query')
 export const CastParam = CastParameterDecoratorFactory('req', 'params')
 export const CastBody = CastParameterDecoratorFactory('req', 'body')
+
+
+export const CastQueryInfer = CastParameterDecoratorInferFactory('req', 'query')
+export const CastParamInfer = CastParameterDecoratorInferFactory('req', 'params')
+export const CastBodyInfer = CastParameterDecoratorInferFactory('req', 'body')
