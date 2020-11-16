51f38ef519625a332977831c636a0ecf9cbb9179
Ofir Genish <ofir3322@gmail.com>
2020-11-17 00:38:25 +0200

    merge CastParameterDecoratorFactory and CastParameterDecoratorInferFactory;

--- packages/nestjs-tunnel-cast/lib/parameter-decorators/common-cast-parameter-decorator.ts
+++ packages/nestjs-tunnel-cast/lib/parameter-decorators/common-cast-parameter-decorator.ts
@@ -1,13 +1,7 @@
 import { 
     CastParameterDecoratorFactory,
-    CastParameterDecoratorInferFactory
 } from "./cast.parameter-decorator.factory";
 
-export const CastQuery = CastParameterDecoratorFactory('req', 'query')
-export const CastParam = CastParameterDecoratorFactory('req', 'params')
-export const CastBody = CastParameterDecoratorFactory('req', 'body')
-
-
-export const CastQueryInfer = CastParameterDecoratorInferFactory('req', 'query')
-export const CastParamInfer = CastParameterDecoratorInferFactory('req', 'params')
-export const CastBodyInfer = CastParameterDecoratorInferFactory('req', 'body')
+export const CastQuery = CastParameterDecoratorFactory('req.query')
+export const CastParam = CastParameterDecoratorFactory('req.params')
+export const CastBody = CastParameterDecoratorFactory('req.body')
