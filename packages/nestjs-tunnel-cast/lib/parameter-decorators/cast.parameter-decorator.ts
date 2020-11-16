780cab1deebe08cc6f0f2907b35ef86e839d5fbe
Ofir Genish <ofir3322@gmail.com>
2020-11-17 00:39:52 +0200

    extractValue on CastParameterDecorator

--- packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.ts
+++ packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.ts
@@ -1,11 +1,17 @@
 
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { extractValue } from '../utils/extract-value';
+import { CastPipeTransformValue } from '../interfaces/cast-pipe-transform-value';
 
-export const CastParameterDecorator =  <T>(from: 'req' | 'res', extractFieldName?: string) => {
-  const decorator = createParamDecorator<unknown, ExecutionContext, T>(
+export const CastParameterDecorator =  <T>(extractionPath?: string) => {
+  const decorator = createParamDecorator<unknown, ExecutionContext, CastPipeTransformValue<T>>(
     (data: unknown, ctx: ExecutionContext) => {
-      const reqOrRes = from == 'req' ? ctx.switchToHttp().getRequest() : ctx.switchToHttp().getResponse();
-      return (extractFieldName ? reqOrRes[extractFieldName] : reqOrRes) as T;
+      const target = { 
+        req: ctx.switchToHttp().getRequest(),
+        res: ctx.switchToHttp().getResponse()
+       }
+      const extractedValue = extractValue<T>(target, extractionPath)
+      return { extractedValue, path: extractionPath };
     },
   );
   return decorator
