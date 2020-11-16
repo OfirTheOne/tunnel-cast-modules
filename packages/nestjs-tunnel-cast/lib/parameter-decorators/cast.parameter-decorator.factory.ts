f6ab38a5b978f3676d958b2e6e4d6bde6697b033
Ofir Genish <ofir3322@gmail.com>
2020-11-17 00:37:32 +0200

    merge CastParameterDecoratorFactory and CastParameterDecoratorInferFactory; remove overload;

--- packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.factory.ts
+++ packages/nestjs-tunnel-cast/lib/parameter-decorators/cast.parameter-decorator.factory.ts
@@ -1,26 +1,20 @@
 
 import { Type } from '@nestjs/common'
-import { Request as eRequest, Response as eResponse } from 'express'
 import { CastPipeFactory, CastPipe } from '../pipes'
 import { CastParameterDecorator } from './cast.parameter-decorator'
 
+type CastParamDecorator = <T>(model?: Type<T> | string) => ParameterDecorator 
 
-export function CastParameterDecoratorFactory(from: "req", fieldName: keyof eRequest): <T>(model: Type<T>) => ParameterDecorator;
-export function CastParameterDecoratorFactory(from: "res", fieldName: keyof eResponse): <T>(model: Type<T>) => ParameterDecorator;
-export function CastParameterDecoratorFactory(from: "res"): (model: any) => ParameterDecorator;
-export function CastParameterDecoratorFactory(from: "req"|"res", fieldName?: string): <T>(model: Type<T>) => ParameterDecorator  {
-    return <T>(model: Type<T>) => {
-      const decorator = CastParameterDecorator<T>(from, fieldName)
-      return decorator(CastPipeFactory(model))
+export function CastParameterDecoratorFactory(extractionPath?: string): 
+  CastParamDecorator {
+    return <T>(model?: Type<T> | string) => {
+      const decorator = CastParameterDecorator<T>(extractionPath)
+      if(model != undefined) {
+        return decorator(CastPipeFactory(model))
+      } else {
+        return decorator(CastPipe)
+      }
+
     } 
 }
 
-
-
-export function CastParameterDecoratorInferFactory(from: "req", fieldName: keyof eRequest): ParameterDecorator;
-export function CastParameterDecoratorInferFactory(from: "res", fieldName: keyof eResponse): ParameterDecorator;
-export function CastParameterDecoratorInferFactory(from: "res"): (model: any) => ParameterDecorator;
-export function CastParameterDecoratorInferFactory(from: "req"|"res", fieldName?: string): ParameterDecorator  {
-  const decorator = CastParameterDecorator(from, fieldName)
-  return decorator(CastPipe)
-}
