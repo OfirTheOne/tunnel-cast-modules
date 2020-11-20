773903845d48ee7c38cd298602b279545c43a569
Ofir Genish <ofir3322@gmail.com>
2020-11-21 00:34:34 +0200

    update exports

--- packages/tunnel-cast/lib/common/decorator/field-options/index.ts
+++ packages/tunnel-cast/lib/common/decorator/field-options/index.ts
@@ -1,35 +1,9 @@
 
 
-import { optionsSetter } from '../field-options/option-setter';
-import { assert, _default, required, parsing } from '../field-options/option-wrapper';
+export { OptionSetter } from './option-setter';
+export { Parsing } from './option-wrapper/parsing.decorator';
+export { Required } from './option-wrapper/required.decorator';
+export { Default } from './option-wrapper/default.decorator';
+export { AssertType } from './option-wrapper/assert-type.decorator';
 
 
-
-    // ==== //
-    
-/**
- * @description set the default value of the field 
- */
-export const Default = _default;
-
-/**
- * @description set the required status of the field 
- */
-export const Required = required;
-
-/**
- * @description declare the type (primitive / class) of the origin field value, and assert it
- */
-export const AssertType = assert;
-
-/**
- * @description provide access to the field-type's options object.
- */
-export const OptionSetter = optionsSetter;
-
-
-
-/**
- * @description add parsing function to the field 
- */
-export const Parsing = parsing;
