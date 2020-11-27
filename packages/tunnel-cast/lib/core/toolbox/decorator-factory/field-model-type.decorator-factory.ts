ce29bfd2b5fa6fa00b48bfd83b67f279e845b902
Ofir Genish <ofir3322@gmail.com>
2020-11-28 01:46:03 +0200

    remove comment

--- packages/tunnel-cast/lib/core/toolbox/decorator-factory/field-model-type.decorator-factory.ts
+++ packages/tunnel-cast/lib/core/toolbox/decorator-factory/field-model-type.decorator-factory.ts
@@ -1,11 +1,8 @@
 import "reflect-metadata";
 import { Class } from "../../../utils/model";
 import { BaseFieldOptions } from "../../../interfaces/public/field-options";
-// import { FieldTypeId } from "../../../internal/field-handler/model-handler";
 import { embedMetadata } from "../../internal/model-metadata/embed-metadata";
 import { extractRootRepo } from "../../internal/model-metadata/extract-metadata";
-// import { FieldOptionProcessor } from '../../../core/toolbox/field-option-processor'
-
 import { TypeRegistry } from '../type-registry'
 
 export function FieldModelTypeDecoratorFactory<T>(typeHandlerId: string | symbol ,options?: BaseFieldOptions, model?: Class): PropertyDecorator {
