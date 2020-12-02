64f1266cf9952ec62687887f32c6f5f3702a7ab4
Ofir Genish <ofir3322@gmail.com>
2020-12-03 01:18:48 +0200

    support BaseFieldEmbeddedData with both typeHandlerId, provider fields

--- packages/core/lib/interfaces/field-embedded-data.ts
+++ packages/core/lib/interfaces/field-embedded-data.ts
@@ -1,10 +1,17 @@
+import { RegisteredTypeProvider } from "../type-registry/registered-type-provider";
+import { BaseFieldOptions } from "./field-options";
 
-
-import { BaseFieldOptions } from './field-options';
-
-export interface FieldEmbeddedData {
+export interface BaseFieldEmbeddedData {
     fieldKey: string;
     options: BaseFieldOptions;
-    typeHandlerId: string | symbol;
     handlerArgs?: Array<any>;
 }
+
+export interface TypeHandlerIdBaseFieldEmbeddedData extends BaseFieldEmbeddedData {
+    typeHandlerId: string | symbol;
+}
+export interface TypeProviderBaseFieldEmbeddedData extends BaseFieldEmbeddedData {
+    provider: RegisteredTypeProvider;
+}
+
+export type FieldEmbeddedData = TypeProviderBaseFieldEmbeddedData | TypeHandlerIdBaseFieldEmbeddedData;
