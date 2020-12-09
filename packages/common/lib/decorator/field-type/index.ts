712353861d3f4de87b32e5df18d0409bde0fd881
Ofir Genish <ofir3322@gmail.com>
2020-12-09 23:42:20 +0200

    add aliases to decorators

--- packages/common/lib/decorator/field-type/index.ts
+++ packages/common/lib/decorator/field-type/index.ts
@@ -9,17 +9,37 @@ import { model } from "./model";
  */
 export const Number = number;
 /**
+ * @description declare a Number type
+ * @alias Number
+ */
+export const Num = number;
+/**
  * @description declare a String type
  */
 export const String = string;
+/**
+ * @description declare a String type
+ * @alias String
+ */
+export const Str = string;
 /**
  * @description declare a Boolean type
  */
 export const Boolean = boolean;
 /**
+ * @description declare a Boolean type
+ * @alias Boolean
+ */
+export const Bool = boolean;
+/**
  * @description declare a Array type
  */
 export const Array = array;
+/**
+ * @description declare a Array type
+ * @alias Array
+ */
+export const Arr = array;
 /**
  * @description declare a Model type, inferred the mode class from the field type definition.
  */
