4b6c4237c4dfe4474d4e40161fef194277b2c4c4
Ofir Genish <ofir3322@gmail.com>
2020-12-09 23:42:33 +0200

    export Validations and FallbackAttribute

--- packages/common/lib/decorator/field-options/index.ts
+++ packages/common/lib/decorator/field-options/index.ts
@@ -3,3 +3,5 @@ export { Parsing } from "./option-wrapper/parsing.decorator";
 export { Required } from "./option-wrapper/required.decorator";
 export { Default } from "./option-wrapper/default.decorator";
 export { AssertType } from "./option-wrapper/assert-type.decorator";
+export { Validations } from "./option-wrapper/validations.decorator";
+export { FallbackAttribute } from './option-wrapper/fallbackAttribute.decorator';
