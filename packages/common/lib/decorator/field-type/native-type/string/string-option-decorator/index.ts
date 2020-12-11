bce07cda0a7e3c8c54601a0bf32165d5c129324c
Ofir Genish <ofir3322@gmail.com>
2020-12-11 02:06:13 +0200

    create type options decorators

---
+++ packages/common/lib/decorator/field-type/native-type/string/string-option-decorator/index.ts
@@ -0,0 +1,22 @@
+import { FieldOptionSetterDecoratorFactory } from "@tunnel-cast/core/decorator-factory/field-option-setter.decorator-factory";
+
+/**
+ * @description declare the type (primitive / class) of the origin field value, and assert it
+ */
+export const Format = function (value) {
+    return FieldOptionSetterDecoratorFactory("format", value);
+};
+
+
+/**
+ * @description declare the type (primitive / class) of the origin field value, and assert it
+ */
+export const Enums = function (value) {
+    return FieldOptionSetterDecoratorFactory("enums", value);
+};
+
+
+export const stringOptionDecorators = {
+    Format,
+    Enums
+}
