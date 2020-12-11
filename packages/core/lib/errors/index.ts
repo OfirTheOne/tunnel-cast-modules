b1b27c11c991d1537cc75e4578793b184293ffc6
Ofir Genish <ofir3322@gmail.com>
2020-12-11 20:36:33 +0200

    place all error classes in errors/index.ts

--- packages/core/lib/errors/index.ts
+++ packages/core/lib/errors/index.ts
@@ -1,5 +1,28 @@
-import { ErrorCode } from "./error-code.enum";
-import { TunnelCastError, CastingError, ErrorWrapper } from "./super-class-error";
+import { ErrorCode } from "./../enums/error-code.enum";
+
+export class TunnelCastError extends Error {
+    constructor(message: string, public code: string | number) {
+        super(message);
+    }
+}
+
+export class CastingError extends TunnelCastError {
+    constructor(message: string, code: string | number, public description: string) {
+        super(message, code);
+    }
+
+    toJSON() {
+        return {
+            message: this.message,
+            code: this.code,
+            description: this.description,
+        };
+    }
+}
+
+export interface ErrorWrapper {
+    originError: any;
+}
 
 export class ModelMetadataRepoNotFoundError extends TunnelCastError {
     constructor(modelName: string) {
