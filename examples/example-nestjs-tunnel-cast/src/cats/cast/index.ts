0d05e7235e9a2579084694ebba7c3b2dd69554ea
Ofir Genish <ofir3322@gmail.com>
2020-12-14 18:43:02 +0200

    add more options

--- examples/example-nestjs-tunnel-cast/src/cats/cast/index.ts
+++ examples/example-nestjs-tunnel-cast/src/cats/cast/index.ts
@@ -1,16 +1,33 @@
-import { Number, Required } from '@tunnel-cast/common';
+import { String, Number, Required, Default, Parsing } from '@tunnel-cast/common';
 
 
 export namespace GetAllCats {
 
     export class Query {
         @Required(false)
+        @Default(10)
+        @Number.Min(1)
+        @Number.Max(10)
+        @Parsing(numString => global.Number(numString))
         @Number()
-        @Number.Max(30)
         limit: number;
 
+        @Required(false)
+        @Default(0)
+        @Number.Min(0)
+        @Parsing(numString => global.Number(numString))
         @Number()
         skip: number;
         
+    }
+
+}
+export namespace GetCatsByType {
+
+    export class Params {
+        @Required(true)
+        @String.Enums(['hose_cat', 'bobcat', 'tiger'])
+        @String()
+        type: string;
     }
 }
