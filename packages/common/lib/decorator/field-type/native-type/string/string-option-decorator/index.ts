c7872963c3a07682be9fcc53c04f8412a6cbfa3e
Ofir Genish <ofir3322@gmail.com>
2020-12-18 17:56:15 +0200

    add string-native-validation decorators

--- packages/common/lib/decorator/field-type/native-type/string/string-option-decorator/index.ts
+++ packages/common/lib/decorator/field-type/native-type/string/string-option-decorator/index.ts
@@ -1,20 +1,45 @@
 import { FieldOptionSetterDecoratorFactory } from "@tunnel-cast/core/decorator-factory/field-option-setter.decorator-factory";
 
-/**
- * @description declare the type (primitive / class) of the origin field value, and assert it
- */
+
 export const Format = function (value) {
     return FieldOptionSetterDecoratorFactory("format", value);
 };
 
-/**
- * @description declare the type (primitive / class) of the origin field value, and assert it
- */
 export const Enums = function (value) {
     return FieldOptionSetterDecoratorFactory("enums", value);
 };
 
+export const MinLength = function (value) {
+    return FieldOptionSetterDecoratorFactory("minLength", value);
+};
+
+export const MaxLength = function (value) {
+    return FieldOptionSetterDecoratorFactory("maxLength", value);
+};
+
+export const StartsWith = function (value) {
+    return FieldOptionSetterDecoratorFactory("startsWith", value);
+};
+
+export const EndsWith = function (value) {
+    return FieldOptionSetterDecoratorFactory("endsWith", value);
+};
+
+export const NotIn = function (value) {
+    return FieldOptionSetterDecoratorFactory("notIn", value);
+};
+
+
+
+
+
+
 export const stringOptionDecorators = {
     Format,
     Enums,
+    MinLength,
+    MaxLength,
+    StartsWith,
+    EndsWith,
+    NotIn,
 };
