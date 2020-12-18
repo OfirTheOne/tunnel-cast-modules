bbd9b2b9b4b46568ee267d9dceba3b9c34b0adea
Ofir Genish <ofir3322@gmail.com>
2020-12-18 17:55:01 +0200

    add more validations;

--- packages/common/lib/decorator/field-type/native-type/string/i-string-options.ts
+++ packages/common/lib/decorator/field-type/native-type/string/i-string-options.ts
@@ -4,4 +4,9 @@ import { BaseFieldOptions } from "@tunnel-cast/core/interfaces/field-options";
 export interface StringFieldOptions extends BaseFieldOptions {
     format?: string | RegExp;
     enums?: Array<string>;
+    minLength?: number;
+    maxLength?: number;
+    startsWith?: string;
+    endsWith?: string;
+    notIn?: Array<string>;
 }
