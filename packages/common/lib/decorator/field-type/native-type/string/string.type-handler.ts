a84b5569079cfdace55a397e994c46639b40f9c0
Ofir Genish <ofir3322@gmail.com>
2020-12-18 17:54:12 +0200

    export nativeValidations for file

--- packages/common/lib/decorator/field-type/native-type/string/string.type-handler.ts
+++ packages/common/lib/decorator/field-type/native-type/string/string.type-handler.ts
@@ -1,8 +1,8 @@
 import { FieldHandler } from "@tunnel-cast/core/field-handler";
-import { NativeValidationDict } from "@tunnel-cast/core/interfaces/native-validation-dict";
 import { Class } from "@tunnel-cast/core/utils/type-helpers";
 
 import { StringFieldOptions } from "./i-string-options";
+import { nativeValidations } from "./string-native-validation";
 
 export const TypeHandlerId = Symbol("STRING");
 
@@ -11,16 +11,7 @@ export const TypeName = "string";
 export class StringHandler extends FieldHandler<StringFieldOptions> {
     public typeName: string = TypeName;
 
-    nativeValidations: NativeValidationDict<StringFieldOptions> = {
-        format: {
-            condition: (value, format) => new RegExp(format).test(value),
-            message: "format failed",
-        },
-        enums: {
-            condition: (value, enums) => enums.length == 0 || enums.includes(value),
-            message: "enums failed",
-        },
-    };
+    nativeValidations = nativeValidations;
 
     constructor(context: any, fieldName: string, projectedContext: any, parentModelRef: Class) {
         super(context, fieldName, projectedContext, parentModelRef);
