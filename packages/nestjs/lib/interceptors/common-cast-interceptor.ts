8c835e95ed8817b786e13239c86587b939d254ea
Ofir Genish <ofir3322@gmail.com>
2020-12-26 23:10:55 +0200

    fix "param" typo to "params"

--- packages/nestjs/lib/interceptors/common-cast-interceptor.ts
+++ packages/nestjs/lib/interceptors/common-cast-interceptor.ts
@@ -1,6 +1,6 @@
 import { CastInterceptorFactory } from "./cast-interceptor-factory";
 
 export const CastQueryInterceptor = CastInterceptorFactory("query");
-export const CastParamInterceptor = CastInterceptorFactory("param");
+export const CastParamInterceptor = CastInterceptorFactory("params");
 export const CastBodyInterceptor = CastInterceptorFactory("body");
 export const CastHeaderInterceptor = CastInterceptorFactory("headers");
