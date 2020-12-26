2261d1d42076135d81b4e006ac448ad490cdaa4d
Ofir Genish <ofir3322@gmail.com>
2020-12-26 23:14:22 +0200

    add BirdsController to example

--- examples/example-nestjs-tunnel-cast/src/app.module.ts
+++ examples/example-nestjs-tunnel-cast/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
+import { BirdsController } from './birds/birds.controller';
 import { CastModule } from '@tunnel-cast/nestjs/cast.module'
 
 @Module({
   imports: [
     CastModule.forFeature({})
   ],
-  controllers: [CatsController],
+  controllers: [CatsController, BirdsController],
   providers: [],
 })
 export class AppModule {}
