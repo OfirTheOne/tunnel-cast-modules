d8d1a47ba646f6d8ab36cc4d00e74652bec507fd
Ofir Genish <ofir3322@gmail.com>
2020-12-26 23:18:23 +0200

    update example

--- examples/example-nestjs-tunnel-cast/src/cats/cast/index.ts
+++ examples/example-nestjs-tunnel-cast/src/cats/cast/index.ts
@@ -7,18 +7,17 @@ export namespace GetAllCats {
         @Required(false)
         @Default(10)
         @Number.Min(1)
-        @Number.Max(10)
-        @Parsing(numString => global.Number(numString))
+        @Number.Max(15)
+        @Parsing(global.Number)
         @Number()
         limit: number;
 
         @Required(false)
         @Default(0)
         @Number.Min(0)
-        @Parsing(numString => global.Number(numString))
+        @Parsing(global.Number)
         @Number()
-        skip: number;
-        
+        skip: number;        
     }
 
 }
