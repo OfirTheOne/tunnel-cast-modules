8e3695726bfa6eaac2b73c4b1de83d3a29031163
Ofir Genish <ofir3322@gmail.com>
2021-01-31 01:28:18 +0200

    change file names

--- packages/nestjs/lib/parameter-decorators/common-cast-parameter-decorator.ts
+++
@@ -1,6 +0,0 @@
-import { CastParameterDecoratorFactory } from "./cast.parameter-decorator.factory";
-
-export const CastQuery = CastParameterDecoratorFactory("req.query");
-export const CastParam = CastParameterDecoratorFactory("req.params");
-export const CastBody = CastParameterDecoratorFactory("req.body");
-export const CastHeaders = CastParameterDecoratorFactory("req.headers");
