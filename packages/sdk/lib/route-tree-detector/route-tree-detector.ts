0bf4d8fb544af544367f38906295c36868e9a4ea
Ofir Genish <ofir3322@gmail.com>
2021-03-06 02:10:12 +0200

    add jsdoc; add extractControllerRequestHandlersHttpPayload; move interfaces to model folder;

--- packages/sdk/lib/route-tree-detector/route-tree-detector.ts
+++ packages/sdk/lib/route-tree-detector/route-tree-detector.ts
@@ -4,50 +4,73 @@ import { METHOD_METADATA, MODULE_PATH, PATH_METADATA } from '@nestjs/common/cons
 import { NestContainer } from '@nestjs/core/injector/container';
 import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 import { Module } from '@nestjs/core/injector/module';
+import { CAST_HTTP_PAYLOAD_TYPE_LIST } from '@tunnel-cast/nestjs/constants';
+import { HttpPayloadArgumentMetadata } from '@tunnel-cast/nestjs/interfaces/http-payload-argument-metadata';
+import { HttpPayloadType } from '@tunnel-cast/nestjs/enums';
+import {
+    ControllerRoutesHttpPayloadArg,
+    ControllerRoutesInspectionData,
+    ControllerInspectionData,
+    ApplicationInspectionData
+ } from '../models';
 
 
-interface ControllerRoutesInspectionData {
-    httpMethod: string,
-    path: string,
-    reqMethodName: string,
-    reqMethodRef: Function
-}
-
-interface ControllerInspectionData {
-    controllerTypeRef: Function | Type<any>,
-    controllerInstanceRef: any,
-    controllerPath: string,
-    controllerRoutes: Array<ControllerRoutesInspectionData>
-}
-
-interface ApplicationInspectionData {
-    routes: Map<string, ControllerInspectionData>;
-    globalPrefix: string
-}
-
-
-function reflectModulePath(metatype: Type<any>) {
-    return Reflect.getMetadata(MODULE_PATH, metatype)
-}
 function reflectControllerPath(metatype: Type<unknown>): string {
     return Reflect.getMetadata(PATH_METADATA, metatype);
 }
 function reflectMethodRoute(method: Function): string {
     return Reflect.getMetadata(PATH_METADATA, method);
 }
+function reflectMethodHttpPayload(metatype: Type<unknown>, method: string): Array<HttpPayloadArgumentMetadata> {
+    return Reflect.getMetadata(CAST_HTTP_PAYLOAD_TYPE_LIST, metatype.prototype, method);
+}
 function reflectRequestMethod(method: Function): RequestMethod {
-    return Reflect.getMetadata(METHOD_METADATA,method);
+    return Reflect.getMetadata(METHOD_METADATA, method);
 }
 
-
+/**
+ * 
+ * @param app Nest application 
+ * @returns {string} the app global prefix, in not exists return empty string. 
+ */
 function extractAppGlobalPrefix(app: INestApplication): string {
     const { config } = app as any;
     return config?.getGlobalPrefix() || '';
-  }
+}
 
+/**
+ * 
+ * @param ctrlClass controller class
+ * @param method controller class method
+ * @returns {Array<ControllerRoutesHttpPayloadArg>} list of the controller-method's parameters metadata - payload-http-type & model-class. 
+ */
+function extractControllerRequestHandlersHttpPayload(
+    ctrlClass: any,
+    method: Function,
+): Array<ControllerRoutesHttpPayloadArg> {
+    let argPayloadList = reflectMethodHttpPayload(ctrlClass, method.name);
+    if (argPayloadList == undefined) { // if undefined the method is not a request handler
+        return [];
+    }
+
+    argPayloadList = argPayloadList.sort((a, b) => a.argumentIndex - b.argumentIndex)
+    const paramTypes: Array<any> = Reflect.getMetadata('design:paramtypes', ctrlClass.prototype, method.name)
+
+    return argPayloadList.map((arg, i) => ({
+        httpPayloadType: HttpPayloadType[arg.payloadType].toLowerCase(),
+        argumentIndex: arg.argumentIndex,
+        model: paramTypes?.[i]
+    }));
+}
+
+/**
+ * 
+ * @param method controller class method
+ * @returns controller-method's http-method-type & relative path from parent controller.
+ */
 function extractControllerRequestHandlers /* paths & methods*/(
     method: Function,
-) {
+): { method: string, path: string } {
     const routePath = reflectMethodRoute(method);
     if (routePath == undefined) { // if undefined the method is not a request handler
         return undefined;
@@ -61,6 +84,11 @@ function extractControllerRequestHandlers /* paths & methods*/(
     };
 }
 
+/**
+ * 
+ * @param module 
+ * @param containerRef 
+ */
 function extractModuleControllers(module: Module, containerRef: NestContainer): Map<string, InstanceWrapper<object>> {
     const { routes, metatype, relatedModules } = module;
     let allRoutes = new Map(routes);
@@ -78,14 +106,16 @@ function extractModuleControllers(module: Module, containerRef: NestContainer): 
     return allRoutes;
 }
 
+/**
+ * 
+ * @param controllersWrapperMap 
+ */
 function scanModuleRoutes(
     controllersWrapperMap: Map<string, InstanceWrapper>,
 ) {
-
     const controllersMetadataMap = new Map<string, ControllerInspectionData>();
 
     for (let ctrl of (controllersWrapperMap.values())) {
-
         const { instance, metatype } = ctrl;
         const prototype = Object.getPrototypeOf(instance);
         const classMethodNameList = Object.getOwnPropertyNames(prototype);
@@ -94,15 +124,14 @@ function scanModuleRoutes(
             .filter(classMethodName => classMethodName != 'constructor')
             .map<ControllerRoutesInspectionData>(classMethodName => {
                 const classMethod = prototype[classMethodName];
-                const paramsTest =  Reflect.getMetadata('design:paramtypes', classMethod)
-
                 const reqHandlerMetadata = extractControllerRequestHandlers(classMethod);
+                const httpPayloadArgs = extractControllerRequestHandlersHttpPayload(metatype, classMethod)
                 if (reqHandlerMetadata) {
                     const { method: httpMethod, path } = reqHandlerMetadata;
-                    return { httpMethod, path, reqMethodName: classMethodName, reqMethodRef: classMethod }
+                    return { httpMethod, path, reqMethodName: classMethodName, reqMethodRef: classMethod, httpPayloadArgs };
                 }
             })
-            .filter(md => md != undefined)
+            .filter(md => md != undefined);
 
         controllersMetadataMap.set(ctrl.name, {
             controllerTypeRef: metatype,
@@ -146,13 +175,22 @@ export function scanApplication(app: INestApplication): ApplicationInspectionDat
 
 }
 
-export function routesDataToJson({ routes , globalPrefix }: ApplicationInspectionData) {
+export function routesDataToJson({ routes, globalPrefix }: ApplicationInspectionData) {
 
     const routesAsList = Array.from(routes.entries())
         .map(([ctrlName, ctrlData]) => ({
             name: ctrlName,
             path: ctrlData.controllerPath,
-            endpoints: ctrlData.controllerRoutes.map(({ reqMethodName, path }) => ({ name: reqMethodName, path }) )
+            endpoints: ctrlData.controllerRoutes
+                .map(({ reqMethodName, path, httpMethod, httpPayloadArgs = [] }) => (
+                    { 
+                        name: reqMethodName, path, 
+                        method: httpMethod, 
+                        payload: httpPayloadArgs?.map(
+                            ({httpPayloadType, model}) => ({ type: httpPayloadType, schemeName: model?.name })
+                        ) 
+                    }
+                ))
         }))
     return {
         globalPrefix,
