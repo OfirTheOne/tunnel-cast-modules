618f98322cccd7e6519ff71eb1a4ab1f3a7f703c
Ofir Genish <ofir3322@gmail.com>
2021-03-20 15:43:16 +0200

    define MessageBuilderFn interface

---
+++ packages/tunnel-cast/lib/models/interfaces/message-builder-fn.ts
@@ -0,0 +1,12 @@
+
+import { FieldConstraintProcedureOptions } from "./field-constraint-procedure-options";
+
+export interface MessageBuilderFn<A=any> {
+    (messageBuilderFnParams:  { 
+        args: A, 
+        fieldValue: any, 
+        fieldName: string, 
+        path: string,
+        options: FieldConstraintProcedureOptions,
+    }): string
+}
