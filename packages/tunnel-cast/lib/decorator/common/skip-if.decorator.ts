10173f977e1afff12a94797a42496a8e4cf10689
Ofir Genish <ofir3322@gmail.com>
2021-03-21 01:52:55 +0200

    if condition bug

--- packages/tunnel-cast/lib/decorator/common/skip-if.decorator.ts
+++ packages/tunnel-cast/lib/decorator/common/skip-if.decorator.ts
@@ -2,7 +2,7 @@ import { decoratorAdapter } from "../../core/factory/decorator-adapter";
 import { FieldConditionalHandlingProcedure } from "../../core/field-decorator-procedure/field-conditional-handling.procedure";
 
 export const SKIP_IF = "skip_if";
-export const skipIf = ({ fieldValue, fieldName ,args, context }) => args.cond(fieldValue, fieldName, context)
+export const skipIf = ({ fieldValue, fieldName ,args, context }) => !args.cond(fieldValue, fieldName, context)
 
 export function SkipIf(cond: ((value, name, context) => boolean)) {
     const adaptee = new FieldConditionalHandlingProcedure(
