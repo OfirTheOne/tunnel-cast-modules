038618de5bd8391a238f7269ae609c28ec2eae13
Ofir Genish <ofir3322@gmail.com>
2021-03-20 03:06:39 +0200

    format;

--- packages/tunnel-cast/lib/core/cast/cast.ts
+++ packages/tunnel-cast/lib/core/cast/cast.ts
@@ -5,38 +5,38 @@ import { FieldProcedureType } from "../../models/enums/field-procedure-type.enum
 import { CastResult } from "../../models/interfaces/cast-result";
 import { executeCastProcedures } from "./execute-cast-procedures";
 
-export function cast<T>(model: (new (...args: any[]) => T) , target: any, options?: any): CastResult<T> {
+export function cast<T>(model: (new (...args: any[]) => T), target: any, options?: any): CastResult<T> {
 
     const modelPrototype = model.prototype;
     const projectedContext = {};
     const decoratedModelFieldsSet: Map<string, number> = Reflect.getMetadata(CAST_DECORATED_FIELDS_LIST, modelPrototype);
     const orderedDecoratedFields = Array
-        .from(decoratedModelFieldsSet?.entries()||[])
-        .sort(([,v1], [,v2]) => v1 - v2)
+        .from(decoratedModelFieldsSet?.entries() || [])
+        .sort(([, v1], [, v2]) => v1 - v2)
         .map(([v]) => v);
-    
+
     const castProceduresListEntries: Array<[string, FieldProcedure[]]> = orderedDecoratedFields
         .map(field => {
             const faList: Array<FieldProcedure> = Reflect.getMetadata(CAST_FIELD_METADATA, modelPrototype, field);
-            faList.forEach(fa => fa.contextRef = target); 
+            faList.forEach(fa => fa.contextRef = target);
             return [field, faList];
         })
-    
+
     const allCastResult = castEngine(castProceduresListEntries, target, projectedContext, options);
 
-    const messages = allCastResult.flat().map(({message}) => message);
+    const messages = allCastResult.flat().map(({ message }) => message);
     const resolvedValue = messages.length ? undefined : Object.assign(new model(), projectedContext)
-    return { resolvedValue, messages};
+    return { resolvedValue, messages };
 }
 
 export function castEngine(castProceduresListEntries: Array<[string, FieldProcedure[]]>, target: any, projectedContext: any, options?: any) {
     const messagesLists = castProceduresListEntries.map(([field, proceduresList]) => {
         const proceduresListGroups = proceduresList
-            .reduce((gr, procedure) => { 
-                gr[procedure.fieldProcedureType] = [...(gr[procedure.fieldProcedureType] || []), procedure]; 
-                return gr; 
-            } ,{} as Record<FieldProcedureType, FieldProcedure[]>);
-        
+            .reduce((gr, procedure) => {
+                gr[procedure.fieldProcedureType] = [...(gr[procedure.fieldProcedureType] || []), procedure];
+                return gr;
+            }, {} as Record<FieldProcedureType, FieldProcedure[]>);
+
         const proceduresExecutionResult = executeCastProcedures(field, proceduresListGroups, target, projectedContext, options);
         return proceduresExecutionResult;
     });
