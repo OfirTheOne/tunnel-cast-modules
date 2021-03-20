964d9c400b0943dc2d14b351331c96cbda3d036e
Ofir Genish <ofir3322@gmail.com>
2021-03-21 01:53:17 +0200

    format

--- packages/tunnel-cast/lib/decorator/common/is-type-of.decorator.ts
+++ packages/tunnel-cast/lib/decorator/common/is-type-of.decorator.ts
@@ -1,18 +1,16 @@
 import { decoratorAdapter } from "../../core/factory/decorator-adapter";
 import { FieldConstraintProcedure } from "../../core/field-decorator-procedure/field-constraint.procedure";
 import { FieldConstraintProcedureOptions } from "../../models/interfaces/field-constraint-procedure-options";
-import {FieldConstraintFn  } from "../../models/interfaces/field-constraint-fn";
-const IS_TYPEOF = "is_typeof";
+import { FieldConstraintFn } from "../../models/interfaces/field-constraint-fn";
 
-
-
-export const isTypeof: FieldConstraintFn<{typeofString: string}> = ({ fieldValue, args }) => typeof fieldValue == args.typeofString;
+export const IS_TYPEOF = "is_typeof";
+export const isTypeof: FieldConstraintFn<{ typeofString: string }> = ({ fieldValue, args }) => typeof fieldValue == args.typeofString;
 
 export function IsTypeof(typeofString: string, options?: FieldConstraintProcedureOptions) {
     const adaptee = new FieldConstraintProcedure(
         IS_TYPEOF,
         options,
-        {typeofString},
+        { typeofString },
         isTypeof,
         ({ fieldName, args }) => `The value ${args.typeofString} is not equals to 'typeof' the field ${fieldName} is `
     );
