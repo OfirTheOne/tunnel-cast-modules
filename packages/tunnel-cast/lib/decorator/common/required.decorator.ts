2e9fb7bc739750afa452501f5d7f344b22106165
Ofir Genish <ofir3322@gmail.com>
2021-03-20 15:45:27 +0200

    return the proper err message on iterate

--- packages/tunnel-cast/lib/decorator/common/required.decorator.ts
+++ packages/tunnel-cast/lib/decorator/common/required.decorator.ts
@@ -2,15 +2,19 @@
 import { globalSetting } from "../../core/globals/globals";
 import { decoratorAdapter } from "../../core/factory/decorator-adapter";
 import { FieldConstraintProcedure } from "../../core/field-decorator-procedure/field-constraint.procedure";
+import { FieldConstraintProcedureOptions } from "../../models/interfaces/field-constraint-procedure-options";
+import { MessageBuilderFn } from "../../models/interfaces/message-builder-fn";
 
 export const REQUIRED = "required";
-export const required = ({ fieldValue, fieldName ,args }) => !globalSetting.defaultEmptyIdentifier({fieldValue} as any);
-export const requiredMessageBuilder = ({ fieldName }) => `The field ${fieldName} is required.`;
+export const required = ({ fieldValue }) => !globalSetting.defaultEmptyIdentifier({fieldValue} as any);
+export const requiredMessageBuilder: MessageBuilderFn = ({ fieldName, options }) => options.iterate ?
+    `Each value in the field ${fieldName} is required.` :
+    `The field ${fieldName} is required.`;
 
-export function Required() {
+export function Required(options?: FieldConstraintProcedureOptions) {
     const adaptee = new FieldConstraintProcedure(
         REQUIRED,
-        {},
+        options||{},
         {},
         required,
         requiredMessageBuilder
