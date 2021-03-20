129d7a04934e9da376781432760cc0b59b49a6f2
Ofir Genish <ofir3322@gmail.com>
2021-03-20 15:44:30 +0200

    use MessageBuilderFn in constructor; add default to options;

--- packages/tunnel-cast/lib/core/field-decorator-procedure/field-constraint.procedure.ts
+++ packages/tunnel-cast/lib/core/field-decorator-procedure/field-constraint.procedure.ts
@@ -1,7 +1,8 @@
 import { FieldProcedure } from "../../models/interfaces/field-procedure";
 import { FieldProcedureType } from "../../models/enums/field-procedure-type.enum";
 import { FieldConstraintProcedureOptions } from "../../models/interfaces/field-constraint-procedure-options";
-import { FieldConstraintFn } from "lib/models/interfaces/field-constraint-fn";
+import { FieldConstraintFn } from "../../models/interfaces/field-constraint-fn";
+import { MessageBuilderFn } from "../../models/interfaces/message-builder-fn";
 
 export class FieldConstraintProcedure<A = any> implements FieldProcedure {
     public readonly fieldProcedureType: FieldProcedureType = FieldProcedureType.Constraint;
@@ -10,10 +11,10 @@ export class FieldConstraintProcedure<A = any> implements FieldProcedure {
 
     constructor(
         public readonly procedureId: string,
-        public options: FieldConstraintProcedureOptions,
-        public args: A,
+        public options: FieldConstraintProcedureOptions = {},
+        public args: A = ({} as any),
         public readonly constraint: FieldConstraintFn<A>,
-        public readonly messageBuilder: string | ((messageBuilderFnParams:  { args: A, fieldValue: any, fieldName: string, path: string}) => string)
+        public readonly messageBuilder: string | MessageBuilderFn<A>
     ) {
 
     }
