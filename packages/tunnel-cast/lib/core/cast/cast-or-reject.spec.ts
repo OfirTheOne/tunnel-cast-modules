c6f44bc229bbdf68af737092e7c0c28b8ae9ad89
Ofir Genish <ofir3322@gmail.com>
2021-03-21 01:52:02 +0200

    create a test for castOrReject

--- packages/tunnel-cast/lib/core/cast/cast-or-reject.spec.ts
+++ packages/tunnel-cast/lib/core/cast/cast-or-reject.spec.ts
@@ -1,1 +1,41 @@
+
+
+
+const mockCast = jest.fn()
+jest.mock('./cast', () => {
+    return {
+        cast: mockCast
+    }
+});
+
+import { castOrReject } from "./cast-or-reject";
+
+describe("castOrReject", () => {
+
+    afterEach(() => jest.resetAllMocks())
+
+    it("should call castOrReject and throw messages list.", () => {
+        const messages = ["error-message-01"];
+        mockCast.mockReturnValueOnce({ messages, resolvedValue: { name: "bob" } });
+        const model: any = { thisIsModel: true } , target = { thisIsTarget: true } , options = { thisIsOptions: true };
+        expect( () => castOrReject(model, target, options)).toThrow(new Error(messages as any));
+        expect(mockCast).toBeCalledTimes(1);
+        expect(mockCast).toBeCalledWith(model, target, options);
+    })
+
+    it("should call castOrReject and return the resolvedValue.", () => {
+        const messages = [];
+        const resolvedValue = { name: "bob" };
+        mockCast.mockReturnValueOnce({ messages, resolvedValue });
+        const model: any = { thisIsModel: true } , target = { thisIsTarget: true } , options = { thisIsOptions: true };
+        expect(castOrReject(model, target, options)).toEqual(resolvedValue);
+        expect(mockCast).toBeCalledTimes(1);
+        expect(mockCast).toBeCalledWith(model, target, options);
+    })
+    
+    afterAll(() => jest.resetModules())
+})
+
 test.todo("castOrReject")
+
+
