b44fdce2e935a2f70c7f61a65511fb68beaa9f9c
Ofir Genish <ofir3322@gmail.com>
2021-03-22 02:02:13 +0200

    add more test cases

--- packages/tunnel-cast/lib/core/cast/cast.spec.ts
+++ packages/tunnel-cast/lib/core/cast/cast.spec.ts
@@ -1,14 +1,14 @@
 
 import { cast } from "./cast";
-import { IsString } from "../../decorator/type/is-string";
+import { IsString, isStringMessageBuilder } from "../../decorator/type/is-string";
 import { IsMatch } from "../../decorator/common/is-match.decorator";
 import { IsNumber } from "../../decorator/type/is-number";
 
 import { SkipIf } from "../../decorator/common/skip-if.decorator";
 import { Required, requiredMessageBuilder } from "../../decorator/common/required.decorator";
-import { IsEmail } from "../../decorator/common/is-email.decorator";
+import { IsEmail, isEmailMessageBuilder } from "../../decorator/common/is-email.decorator";
 import { Nullable } from "../../decorator/common/nullable.decorator";
-
+import { Length } from "../../decorator/common/length.decorator";
 
 test.todo("cast test on SkipIf & Nullable");
 test.todo("cast test on Default");
@@ -39,17 +39,37 @@ describe("cast high level expected behavior.", () => {
         listOfStuff: Array<any>;
     }
 
-    it("ExampleDTO01 general usage testing, focus on constraint 'iterate'", () => {
+    it("ExampleDTO01 general usage testing, focus on constraint 'iterate' behavior, casting pass.", () => {
+        const providedValue = { 
+            myEmail: "autor@gmail.com",
+            myAge: 30, 
+            myId: "12312332", 
+            listOfStuff: [1, 2, "123"],
+            blah: "blah",
+        }
+        const expectedValue = providedValue;
+        const { messages, resolvedValue } = cast(
+            ExampleDTO01, 
+            providedValue
+        );
+        
+        expect(messages.length).toEqual(0);
+        expect(resolvedValue).toEqual(expectedValue);
+    })
+
+
+    it("ExampleDTO01 general usage testing, focus on constraint 'iterate' behavior, casting fail.", () => {
 
         const expectedErrMessages = [
+            isEmailMessageBuilder({fieldName: 'myEmail', options: {} } as any),
             requiredMessageBuilder({fieldName: 'listOfStuff', options: { iterate: true } } as any),
             requiredMessageBuilder({fieldName: 'blah', options: {} } as any),
-        ]
+        ];
 
         const { messages, resolvedValue } = cast(
             ExampleDTO01, 
             { 
-                myEmail: "autor@gmail.com",
+                myEmail: "autor@gamil.com",
                 myAge: 30, 
                 myId: "12312332", 
                 listOfStuff: [1, undefined, "123"] 
@@ -58,7 +78,68 @@ describe("cast high level expected behavior.", () => {
         
         expect(resolvedValue).toBeUndefined();
         expect(messages).toBeDefined();
-        expect(messages.length).toEqual(2);
+        expect(messages.length).toEqual(3);
+        expectedErrMessages.forEach(msg => expect(messages).toContain(msg));
+    })
+
+
+    class ExampleDTO02 {
+        @Required()
+        importantMessage: string;
+
+        @Nullable()
+        @Required()
+        @Length(5, 10)
+        dumbMessage: any;
+
+        @SkipIf((v, k, context) => context.firstName === "madona" )
+        @IsString() 
+        lastName: string;
+
+        @IsString() 
+        firstName: string;
+
+    }
+
+    it("ExampleDTO02 general usage testing, focus on Required & Nullable behavior, casting pass.", () => {
+
+        const providedValue = { 
+            importantMessage: "cast can make things easier",
+            dumbMessage: undefined, 
+            lastName: undefined, 
+            firstName: "madona"
+        }
+        const expectedValue = providedValue;
+
+        const { messages, resolvedValue } = cast(
+            ExampleDTO02, 
+            providedValue
+        );
+        
+        expect(messages.length).toEqual(0);
+        expect(resolvedValue).toEqual(expectedValue);
+    })
+
+    it("ExampleDTO02 general usage testing, focus on Required & Nullable behavior, casting fail.", () => {
+        const expectedErrMessages = [
+            isStringMessageBuilder({fieldName: 'lastName', options: {} } as any),
+        ];
+
+        const providedValue = { 
+            importantMessage: 123,
+            dumbMessage: undefined, 
+            lastName: undefined, 
+            firstName: "kadona"
+        };
+
+        const { messages, resolvedValue } = cast(
+            ExampleDTO02, 
+            providedValue
+        );
+        
+        expect(resolvedValue).toBeUndefined();
+        expect(messages).toBeDefined();
+        expect(messages.length).toEqual(1);
         expectedErrMessages.forEach(msg => expect(messages).toContain(msg));
     })
 
