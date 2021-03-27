d1442e8b812d60f3773789aaeafe9e4c0319aff7
Ofir Genish <ofir3322@gmail.com>
2021-03-27 15:07:17 +0300

    implement map decorator

--- packages/tunnel-cast/lib/decorator/common/map.decorator.ts
+++ packages/tunnel-cast/lib/decorator/common/map.decorator.ts
@@ -3,3 +3,34 @@
  * @decorator_type **FieldParserProcedure**
  * 
  */
+
+import { FieldParserFn } from "../../models/interfaces/field-parser-fn";
+import { decoratorAdapter } from "../../core/factory/decorator-adapter";
+import { FieldParserProcedure } from "../../core/field-decorator-procedure/field-parser.procedure";
+import { FieldParserProcedureOptions } from "../../models/interfaces/field-parser-procedure-options";
+
+export const MAP = "map";
+export const map: FieldParserFn = ({ fieldValue, args, context }) => {
+    return (
+        Array.isArray(fieldValue) && 
+        typeof args.mapper == 'function' 
+    ) ?
+        fieldValue.map( (v, i)  => args.mapper(v, i, context)) :
+        fieldValue;
+};
+
+/**
+ * @decorator_type **FieldParserProcedure**
+ * 
+ * @param mapper 
+ * @param options 
+ */
+export function Map(mapper:  ((v: any, i: number, context: any) => any),  options?: FieldParserProcedureOptions) {
+    const adaptee = new FieldParserProcedure(
+        MAP,
+        options||{},
+        { mapper },
+        map,
+    );
+    return decoratorAdapter(adaptee);
+}
