b3557ee9f3a1ea6e648d57e35ab756c162b7cdfd
Ofir Genish <ofir3322@gmail.com>
2021-03-27 16:42:43 +0300

    add abstract field options to FieldProcedure

--- packages/tunnel-cast/lib/models/interfaces/field-procedure.ts
+++ packages/tunnel-cast/lib/models/interfaces/field-procedure.ts
@@ -1,4 +1,5 @@
 import { FieldProcedureType } from "../../models/enums/field-procedure-type.enum";
+import { FieldProcedureOptions } from "./field-procedure-options";
 
 export abstract class FieldProcedure {
 
@@ -7,6 +8,8 @@ export abstract class FieldProcedure {
     public abstract procedureId: string;
 
     public abstract fieldName: string;
+    
+    public abstract options: FieldProcedureOptions;
     // public abstract contextRef: any;
   
 }
