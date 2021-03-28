c6b4c17c527dd47f4af1ce86b73a651b9f32c184
Ofir Genish <ofir3322@gmail.com>
2021-03-29 02:00:14 +0300

    implement IsEquals

---
+++ packages/tunnel-cast/lib/decorator/common/is-equals.decorator.ts
@@ -0,0 +1,45 @@
+import { deepStrictEqual } from "assert";
+
+import { FieldConstraintProcedureOptions } from "../../models/interfaces/field-constraint-procedure-options";
+import { decoratorAdapter } from "../../core/factory/decorator-adapter";
+import { FieldConstraintProcedure } from "../../core/field-decorator-procedure/field-constraint.procedure";
+import { FieldConstraintFn } from "../../models/interfaces/field-constraint-fn";
+import { MessageBuilderFn } from "../../models/interfaces/message-builder-fn";
+
+export const IS_EQUALS = "is-equals";
+export const isEquals: FieldConstraintFn<{ value: any, strict: boolean }> = ({ fieldValue, args }) => {
+    if(args.strict == false) {
+        return args.value == fieldValue
+    } else {
+        try {
+            deepStrictEqual(fieldValue, args.value);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+};
+export const isEqualsMessageBuilder: MessageBuilderFn = ({ fieldName, options }) => options.iterate ?
+    `Some values in the field ${fieldName} are not a pass assertion.` :
+    `The field ${fieldName} is not pass assertion.`;
+
+/**
+ * @decorator_type **FieldConstraintProcedure**
+ * 
+ * @param options 
+ */
+export function IsEquals(value: any, strict: boolean , options?: FieldConstraintProcedureOptions): PropertyDecorator;
+export function IsEquals(value: any, options?: FieldConstraintProcedureOptions): PropertyDecorator;
+export function IsEquals(value: any, strictOrOptions: (boolean | FieldConstraintProcedureOptions), options?: FieldConstraintProcedureOptions) {
+    
+    const strict = typeof strictOrOptions == 'boolean' ? strictOrOptions : true;
+    
+    const adaptee = new FieldConstraintProcedure(
+        IS_EQUALS,
+        options||{},
+        { value, strict },
+        isEquals,
+        isEqualsMessageBuilder
+    );
+    return decoratorAdapter(adaptee);
+}
