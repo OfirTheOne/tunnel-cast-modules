e4769c0fac256e4b00376d35d1be25e2bf486694
Ofir Genish <ofir3322@gmail.com>
2021-04-01 20:30:55 +0300

    implement is-object

---
+++ packages/tunnel-cast/lib/decorator/type/is-object.decorator.ts
@@ -0,0 +1,23 @@
+import { FieldConstraintProcedureOptions } from "../../models/interfaces/field-constraint-procedure-options";
+import { decoratorAdapter } from "../../core/factory/decorator-adapter";
+import { FieldConstraintProcedure } from "../../core/field-decorator-procedure/field-constraint.procedure";
+
+
+const IS_OBJECT = "is_object";
+
+const isObject = ({ fieldValue }) => (
+    fieldValue != null && 
+    fieldValue != undefined && 
+    typeof fieldValue == 'object'
+);
+
+export function IsObject(options?: FieldConstraintProcedureOptions) {
+    const adaptee = new FieldConstraintProcedure(
+        IS_OBJECT,
+        options,
+        {},
+        isObject,
+        ({ fieldName }) => `The field ${fieldName} is not of type object`
+    );
+    return decoratorAdapter(adaptee);
+}
