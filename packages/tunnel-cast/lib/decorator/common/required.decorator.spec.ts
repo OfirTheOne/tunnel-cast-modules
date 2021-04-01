ea04cc2c419dafaa39a52e0c8b5e19d787939513
Ofir Genish <ofir3322@gmail.com>
2021-04-01 23:23:11 +0300

    add tests for required

--- packages/tunnel-cast/lib/decorator/common/required.decorator.spec.ts
+++ packages/tunnel-cast/lib/decorator/common/required.decorator.spec.ts
@@ -1,1 +1,127 @@
-test.todo("required")
+import { required } from "./required.decorator";
+
+
+
+describe("[constraint fn] required", () => {
+
+    it("constraint return true for empty object.", () => {
+        const fieldName = "testField";
+        const fieldValue = { };
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy();
+    })
+
+    it("constraint return true for the number zero.", () => {
+        const fieldName = "testField";
+        const fieldValue = 0;
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy();
+    })
+
+    it("constraint return false for undefined.", () => {
+        const fieldName = "testField";
+        const fieldValue = undefined;
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy();
+    })
+
+    it("constraint return false for null.", () => {
+        const fieldName = "testField";
+        const fieldValue = null;
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy();
+    })
+
+    it("constraint return false for empty string.", () => {
+        const fieldName = "testField";
+        const fieldValue = '';
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy();
+    })
+
+    it("constraint return true for non empty string.", () => {
+        const fieldName = "testField";
+        const fieldValue = 'hello';
+        const args = {};
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = required({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy();
+    })
+
+})
