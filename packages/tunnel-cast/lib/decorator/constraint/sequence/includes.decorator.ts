c0a1e87dd23ef132d3cc8badc24589a1f9e0011b
Ofir Genish <ofir3322@gmail.com>
2021-04-03 01:53:22 +0300

    implement includes

---
+++ packages/tunnel-cast/lib/decorator/constraint/sequence/includes.decorator.ts
@@ -0,0 +1,23 @@
+import { FieldConstraintProcedureOptions } from "../../../models/interfaces/field-constraint-procedure-options";
+import { decoratorAdapter } from "../../../core/factory/decorator-adapter";
+import { FieldConstraintProcedure } from "../../../core/field-decorator-procedure/field-constraint.procedure";
+import { FieldConstraintFn } from "../../../models/interfaces/field-constraint-fn";
+
+export const INCLUDES = "includes";
+export const includes: FieldConstraintFn<{ value: any }> = ({ fieldValue, args }) => {
+    return (typeof fieldValue == 'string' || Array.isArray(fieldValue) ) ?
+                fieldValue.includes(args.value) : false;
+};
+export const includesMessageBuilder = ({ fieldName }) => `The includes of the field ${fieldName} dose not match the includes constraint.`;
+
+
+export function Includes(value: any, options?: FieldConstraintProcedureOptions) {
+    const adaptee = new FieldConstraintProcedure(
+        INCLUDES,
+        options,
+        { value },
+        includes,
+        includesMessageBuilder
+    );
+    return decoratorAdapter(adaptee);
+}
