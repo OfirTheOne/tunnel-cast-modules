54e48a9cb0fc9761fb6de89c115b9f85069676a4
Ofir Genish <ofir3322@gmail.com>
2021-04-03 22:23:06 +0300

    add test for length

--- packages/tunnel-cast/lib/decorator/constraint/common/length.decorator.spec.ts
+++ packages/tunnel-cast/lib/decorator/constraint/common/length.decorator.spec.ts
@@ -1,1 +1,166 @@
-test.todo("length")
+
+import { length } from "./length.decorator"
+
+describe("[constraint fn] length", () => {
+
+    it("constraint return true for valid len.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            len: 4
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy();
+    })
+
+    it("constraint return false for bad len.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            len: 8
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy();
+    })
+
+    it("constraint return true valid min and max.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            min: 3,
+            max: 5 
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy()
+    })
+    
+    it("constraint return true valid min and max with the same value.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4, 5, 6];
+        const args: any = { 
+            min: 6,
+            max: 6 
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy()
+    })
+    it("constraint return false bad min and max.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            min: 5,
+            max: 6 
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy()
+    })
+
+    it("constraint return true for valid len, ignore min and max.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            min: 7,
+            max: 9,
+            len: 4
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeTruthy();
+    })
+
+    it("constraint return false for bad len, ignore min and max.", () => {
+        const fieldName = "testField";
+        const fieldValue = [1, 2, 3, 4];
+        const args: any = { 
+            min: 3,
+            max: 5,
+            len: 2
+        };
+        const context = {
+            [fieldName]: fieldValue
+        };
+
+        const constraintResult = length({ 
+            args,
+            fieldValue,
+            fieldName,
+            path: fieldName,
+            context, 
+            options: {} 
+        });
+
+        expect(constraintResult).toBeFalsy();
+    })
+
+})
